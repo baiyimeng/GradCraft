0b2cb37dfda00da98b5dc63d5b51dc2125288717
baiyimeng <111271299+baiyimeng@users.noreply.github.com>
2024-04-08 18:25:24 +0800

    Update global_pc_grad_magnitude_positive.py

--- solvers/global_pc_grad_magnitude_positive.py
+++ solvers/global_pc_grad_magnitude_positive.py
@@ -59,43 +59,78 @@ class GlobalPCGradMagnitudePositive():
         self._set_grad(pc_grad)
         return
 
-    def _project_conflicting(self, grads, has_grads, shapes=None):
+    # def _project_conflicting(self, grads, has_grads, shapes=None):
+    #     shared = torch.stack(has_grads).prod(0).bool()
+    #     pc_grads, num_task = copy.deepcopy(grads), len(grads)
+
+    #     for i in range(num_task):
+    #         conflict_task_index = []
+    #         for j in range(num_task):
+    #             if torch.dot(grads[i], grads[j]) < 0:
+    #                 conflict_task_index.append(j)
+    #         if len(conflict_task_index) == 0:
+    #             continue
+    #         elif len(conflict_task_index) == 1:
+    #             j = conflict_task_index[0]
+    #             g_j = grads[j]
+    #             pc_grads[i] -= torch.dot(pc_grads[i], g_j) * g_j / (g_j.norm()**2) - self.e * g_j.norm() * grads[i].norm()
+    #         else:
+    #             conflict_grads = [grads[j] for j in conflict_task_index]
+    #             G = torch.stack(conflict_grads)
+    #             left = torch.matmul(G, G.T)
+    #             right = -torch.matmul(G, grads[i].unsqueeze(1))
+
+    #             conflict_norm = [grads[k].norm() for k in conflict_task_index]
+    #             right_extra = (self.e * grads[i].norm() * torch.stack(conflict_norm)).unsqueeze(1)
+    #             right = right + right_extra
+
+    #             conflict_weights = torch.matmul(torch.inverse(left), right)
+    #             pc_grads[i] += torch.matmul(G.T, conflict_weights).squeeze(-1)
+
+    #     merged_grad = torch.zeros_like(grads[0]).to(grads[0].device)
+    #     if self._reduction:
+    #         merged_grad[shared] = torch.stack([g[shared]
+    #                                        for g in pc_grads]).mean(dim=0)
+    #     elif self._reduction == 'sum':
+    #         merged_grad[shared] = torch.stack([g[shared]
+    #                                        for g in pc_grads]).sum(dim=0)
+    #     else: exit('invalid reduction method')
+
+    #     merged_grad[~shared] = torch.stack([g[~shared]
+    #                                         for g in pc_grads]).sum(dim=0)
+    #     return merged_grad
+
+    def _project_conflicting(self, grads, has_grads):
         shared = torch.stack(has_grads).prod(0).bool()
-        pc_grads, num_task = copy.deepcopy(grads), len(grads)
-
-        for i in range(num_task):
-            conflict_task_index = []
-            for j in range(num_task):
-                if torch.dot(grads[i], grads[j]) < 0:
-                    conflict_task_index.append(j)
-            if len(conflict_task_index) == 0:
-                continue
-            elif len(conflict_task_index) == 1:
-                j = conflict_task_index[0]
-                g_j = grads[j]
-                pc_grads[i] -= torch.dot(pc_grads[i], g_j) * g_j / (g_j.norm()**2) - self.e * g_j.norm() * grads[i].norm()
+        grads = torch.stack(grads)
+        num_task = len(grads)
+        
+        def proj_one(g):
+            inner_products = torch.sum(g * grads, dim = -1)
+            negative_indices = torch.where(inner_products < 0.)[0]
+            if len(negative_indices) == 0:
+                return g
             else:
-                conflict_grads = [grads[j] for j in conflict_task_index]
-                G = torch.stack(conflict_grads)
+                G = torch.gather(input=grads, index=negative_indices.unsqueeze(1).expand(-1, grads.size(1)), dim=0)
                 left = torch.matmul(G, G.T)
-                right = -torch.matmul(G, grads[i].unsqueeze(1))
-
-                conflict_norm = [grads[k].norm() for k in conflict_task_index]
-                right_extra = (self.e * grads[i].norm() * torch.stack(conflict_norm)).unsqueeze(1)
+                right = -torch.matmul(G, g.unsqueeze(1))
+                conflict_norm = torch.norm(G, dim=1)
+                right_extra = (self.e * g.norm() * conflict_norm).unsqueeze(1)
                 right = right + right_extra
-
                 conflict_weights = torch.matmul(torch.inverse(left), right)
-                pc_grads[i] += torch.matmul(G.T, conflict_weights).squeeze(-1)
-
+                res = torch.matmul(G.T, conflict_weights).squeeze(-1)
+                return g + res
+        
+        pc_grads = torch.stack([proj_one(g) for g in grads])
         merged_grad = torch.zeros_like(grads[0]).to(grads[0].device)
         if self._reduction:
             merged_grad[shared] = torch.stack([g[shared]
-                                           for g in pc_grads]).mean(dim=0)
+                                            for g in pc_grads]).mean(dim=0)
         elif self._reduction == 'sum':
             merged_grad[shared] = torch.stack([g[shared]
-                                           for g in pc_grads]).sum(dim=0)
+                                            for g in pc_grads]).sum(dim=0)
         else: exit('invalid reduction method')
-
+    
         merged_grad[~shared] = torch.stack([g[~shared]
                                             for g in pc_grads]).sum(dim=0)
         return merged_grad
